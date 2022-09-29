303f28ee33c04a640d25f80f12e2cf4bf63fb599
Luiz D'Avila <83254624+LuizDavilaa@users.noreply.github.com>
2022-09-29 12:15:50 -0300

    criação de variaveis =)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,10 +8,21 @@ import { ApiService } from '../services/api.service';
 })
 export class HomePage {
 
-  constructor(private api: ApiService) {}
-    buscarTodos(page=1){
-      this.api.buscarUsuarios(page).subscribe(usuarios =>{});
-    }
+  page;
+  perPage;
+  total;
+  totalPages;
+  data;
 
-
+  constructor(private api: ApiService) {this.buscarTodos();}
+  buscarTodos(page = 1) {
+    this.api.buscarUsuarios(page).subscribe((usuarios) => {
+      console.log(usuarios);
+      this.page=usuarios['page'];
+      this.perPage=usuarios['per_page'];
+      this.total=usuarios['total'];
+      this.totalPages=usuarios['total_pages'];
+      this.data=usuarios['data'];
+    });
+  }
 }
